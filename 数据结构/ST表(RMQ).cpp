dc1b766fca5bbf35fa0e32fe015c80e4beec6a57
IndigoRipple <50111061+IndigoRipple@users.noreply.github.com>
2022-09-08 17:02:03 +0800

    Update ST表(RMQ).cpp

--- 数据结构/ST表(RMQ).cpp
+++ 数据结构/ST表(RMQ).cpp
@@ -1,22 +1,25 @@
 //求最大值，初始化和查询取max
 //求最小值，初始化和查询取min
 //原数组下标从1开始使用
-const int N = 5e4+7;
-int dp[N][22];
-int mm[N];
+const int N = 1e5+7;
+int st[N][32];
+int Log2[N];
 //初始化ST表
 void initRMQ(int n,int g[]) { //n数组长度 g数组名称
-	mm[0] = -1;
-	for(int i = 1; i <= n; i++) {
-		mm[i] = ((i&(i-1)) == 0)?mm[i-1]+1:mm[i-1];
-		dp[i][0] = g[i];
+	Log2[1]=0;
+	for(int i=2; i <= n; i++) {
+		Log2[i]=Log2[i-1];
+		if((1 << Log2[i] + 1) == i) {
+			++Log2[i];
+		}
 	}
-	for(int j = 1; j <= mm[n];j++)
-		for(int i = 1; i + (1<<j) -1 <= n; i++)
-			dp[i][j] = max(dp[i][j-1],dp[i+(1<<j-1)][j-1]);//最小值时此处取min
+	for(int i = 1; i <= n; i++)	st[i][0]=g[i];
+	for(int j = 1; j <= 21; j++) 
+		for(int i = 1; (i + (1 << j) - 1) <= n; i++)
+			st[i][j] = max (st[i][j-1], st[i + (1 << (j-1) )][j-1]); //最小值max修改为min
 }
 //查询ST表
-int rmq(int x,int y) { //x左端点 y右端点
-	int k = mm[y-x+1];
-	return max(dp[x][k],dp[y-(1<<k)+1][k]);//最小值时此处取min
+int query_min(int l,int r) { //l左端点 r右端点
+	int len = r - l + 1, k = Log2[len];
+	return max(st[l][k],st[r - (1<<k) + 1][k]); //最小值max修改为min
 }
