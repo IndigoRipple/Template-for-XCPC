4ea5014952f51f265c8c42e8f764cabc3d388088
zwuis <42805402+zwuis@users.noreply.github.com>
2022-11-12 17:32:33 +0800

    Fix ac-automaton

--- 字符串/ac-automaton.cpp
+++ 字符串/ac-automaton.cpp
@@ -14,11 +14,19 @@ for (int i = 0; i < 26; i++) {
 while (!q.empty()) {
     auto qf = q.front();
     q.pop();
-    while (qf.fa) {
-        if (nxt[qf.fa][qf.ch]) {
-            fail[qf.pos] = nxt[qf.fa][qf.ch];
-            break;
-        } else qf.fa = fail[qf.fa];
+    for (int i = 0; i < 26; i++) {
+        if (nxt[qf.pos][i]) {
+            q.push({nxt[qf.pos][i], qf.pos, i});
+        }
     }
-    if (!fail[qf.pos] && nxt[0][qf.ch]) fail[qf.pos] = nxt[0][qf.ch];
+    if (qf.fa) {
+        qf.fa = fail[qf.fa];
+        while (qf.fa) {
+            if (nxt[qf.fa][qf.ch]) {
+                fail[qf.pos] = nxt[qf.fa][qf.ch];
+                break;
+            } else qf.fa = fail[qf.fa];
+        }
+    }
+    if (!fail[qf.pos] && nxt[0][qf.ch] && nxt[0][qf.ch] != qf.pos) fail[qf.pos] = nxt[0][qf.ch];
 }
